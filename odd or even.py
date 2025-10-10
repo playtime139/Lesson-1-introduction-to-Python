3eda608432711b4d2c277c7ebb5c82218d456d87
playtime139 <ryankimungui@gmail.com>
2025-10-11 10:01:52 +1100

    odd or even.py

---
+++ odd or even.py
@@ -0,0 +1,11 @@
+birthday1 = "12-05-1995"
+birthday2 = "23-08-2000"
+birthday3 = "15-01-1992"
+birthday4 = "09-11-1987"
+birthday5 = "30-06-1999"
+
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
